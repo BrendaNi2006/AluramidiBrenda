144e8c86ae05f8a0c4c358b7761f94668d7edfab
BrendaNi2006 <147116797+BrendaNi2006@users.noreply.github.com>
2023-10-23 22:32:09 -0300

    Update main.js

--- Alura mid Brenda /Alura Midi3/main.js
+++ Alura mid Brenda /Alura Midi3/main.js
@@ -1,9 +1,18 @@
-function tocaSomPom (idElemntoAudio) {
+function tocaSom (idElemntoAudio) {
     document.querySelector(idElemntoAudio).play();
 }
 
  const listaDeTeclas = document.querySelectorAll('.tecla');
 
-for (let contador = 0; contador < listaDeTeclas.length; contador++) {}
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    }
+}
 
 
